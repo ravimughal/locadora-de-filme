ed622899e1cc811b5e0ed19756dac92238b26065
felipesphair <107360437+felipesphair@users.noreply.github.com>
2022-06-20 21:48:07 -0300

    aaaaaa
    att

--- main.py
+++ main.py
@@ -3,30 +3,38 @@ import module_filme
 import module_jogos
 
 
+
 print("\033[1;35m-\033[m"*100)
 print("\033[1;35mLazywax\033[m")
 print("\033[1;35mPucpr.BES.2022-1\033[m")
 print()
 
 print("\033[1;35mIntegrantes da equipe:\033[m")
-print("\033[1;35mMateus Augusto Tozin, Julia Engels, Ravi Mughal, João Dadas e Felipe Gabriel\033[m")
+print("\033[1;35mMateus Augusto Tozin, Julia Engels, R1avi Mughal, João Dadas e Felipe Gabriel\033[m")
 print("\033[1;35m-\033[1;34m"*100)
 # ---------------------------------------------------------------------------------------------
-print("\033[1;36mBem vindo ao menu principal!\033[m")
-print()
-print("\033[1;36m[1] Gerenciamento de cliente:\033[m")
-print("\033[1;36m[2] Gerenciamento de Filmes:\033[m")
-print("\033[1;36m[3] Gerenciamento de Jogos:\033[m")
-print("\033[1;36m[4] Gerenciar lançamento:\033[m")
+
+while True:
+
+    print()
+    print("\033[1;36mBem vindo ao menu principal!\033[m")
+    print()
+    print("\033[1;36m[1] Gerenciamento de cliente:\033[m")
+    print("\033[1;36m[2] Gerenciamento de Filmes:\033[m")
+    print("\033[1;36m[3] Gerenciamento de Jogos:\033[m")
+    print("\033[1;36m[4] Sair do programa:\033[m")
 
 
-select = int(input("\033[1;36mSelecionar opção:\033[1;36m"))
 
-if select == 1:
-    module_cadastro.cliente()
-elif select == 2:
-    module_filme.filmes()
-elif select == 3:
-    module_jogos.jogos()
-elif select == 4:
-    print("Em desenvolvimento")
+
+    select = int(input("\033[1;36mSelecionar opção:\033[1;36m"))
+
+    if select == 1:
+        module_cadastro.cliente()
+    elif select == 2:
+        module_filme.filmes()
+    elif select == 3:
+        module_jogos.jogos()
+    if select == 4:
+        print("Nunca é um adeus :)")
+        break
--- module_jogos.py
+++ module_jogos.py
@@ -69,6 +69,84 @@ def jogos():
                 return jogo
         return None
 
+    def listar_jogo():
+        url = './jogo.csv'
+        df = pd.read_csv(url)
+
+        print(df)
+
+    def realizar_emprestimo():
+        print('\n------ EMPRÉSTIMOS ------\n')
+        cpf = input("Digite o cpf do cliente: ")
+        with open('clientes.csv') as clientes_csv:
+            reader_obj = csv.reader(clientes_csv, delimiter=',')
+
+            linhas = 0
+            for coluna in reader_obj:
+                if linhas == 0:
+                    linhas += 1
+                else:
+                    if coluna[0] == cpf:
+                        pesquisado = coluna[1]
+                        print(f"cpf: {pesquisado} | idade: {coluna[2]}")
+                        codigo_jogo = input("Digite seu codigo_jogo: ")
+                        data = input("Digite a data do jogo: ")
+
+                        colunas = ['codigo do jogo', 'cpf', 'nome', 'data']  # colunas da tabela .csv
+                        file_exists = os.path.isfile('emprestimo.csv')
+                        with open('emprestimo.csv', 'a', newline='') as emprestimo_csv:
+                            # DictWriter grava dados no formato de dicionário
+                            cadastrar = csv.DictWriter(
+                                emprestimo_csv, fieldnames=colunas, delimiter=',',
+                                lineterminator='\r\n')  # fieldnames = cpf de campos, ou seja -> colunas, divisor de dados sendo ';', lineterminator \r\n serve para quebrar a linha
+                            # caso não existe o arquivo 'file_exist', faz o fieldnames funcionar, visto que há o 'writeheader()'
+                            if not file_exists:
+                                # writeheader grava a primeira linha de arquivo csv usando os cpfs de campo pré-especificados.
+                                cadastrar.writeheader()
+                            # escrever nas linhas em respectivas 'keys' e 'values', title() -> deixar letra maiuscula
+                            cadastrar.writerow(
+                                {'codigo do jogo': codigo_jogo, 'cpf': cpf, 'nome': pesquisado, 'data': data})
+
+                        print('Cadastro realizado com sucesso!')
+                        return pesquisado
+                    else:
+                        linhas += 1
+            print("pessoa não localizada")
+            return None
+
+    def listar_emprestimos():
+        url = './emprestimo.csv'
+        df = pd.read_csv(url)
+        print(df)
+
+    def checkout():
+        pass
+        cpf = input("Digite o cpf do cliente: ")
+        with open('clientes.csv') as clientes_csv:
+            reader_obj = csv.reader(clientes_csv, delimiter=',')
+
+            linhas = 0
+            for coluna in reader_obj:
+                if linhas == 0:
+                    linhas += 1
+                else:
+                    if coluna[0] == cpf:
+                        pesquisado = coluna[1]
+                        print(f"cpf: {pesquisado} | idade: {coluna[2]}")
+                        codigo_jogo = input("Digite seu codigo_jogo: ")
+                        # colocar situaçao de devolvido
+                        # se nao emprestaram, puxar para emprestimo
+
+    def calc_valor_final():
+        pass
+        # ligar com no emprestimo
+        valor = 0
+        days = int(input('Quantos dias sera alugado: '))
+        if days <= 7:
+            valor += 20
+        elif days > 7:
+            valor += 20 + days * 2
+
     def start(ops, jogos):
         while True:
             op = menu(ops)
@@ -84,6 +162,16 @@ def jogos():
                 if alguem != None:
                     print(alguem)
             elif op == 5:
+                listar_jogo()
+            elif op == 6:
+                realizar_emprestimo()
+            elif op == 7:
+                listar_emprestimos()
+            elif op == 8:
+                checkout()
+            elif op == 9:
+                calc_valor_final()
+            elif op == 10:
                 break
 
     jogos = []
@@ -91,6 +179,11 @@ def jogos():
            "2. Editar jogo",
            "3. Excluir jogo",
            "4. Pesquisar jogo",
-           "5. Sair")
+           "5. Listar jogo",
+           "6. Registrar empréstimo",
+           "7. Listar Empréstimos",
+           "8. Checkout",
+           "9. Valor do Empréstimo"
+           "10. Sair")
     start(ops, jogos)
     print(jogos)
